832e87e5296f1fcf933603f65eaf9887854f2386
feliciakwala <felicia.kwala@gmail.com>
2020-03-07 16:05:18 -0800

    TestRun 1

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,5 +1,13 @@
 package model;
 
 public class Customer {
+    String name;
+    String phoneNumber;
+
+    //EFFECTS: constructs a new customer with given name and phoneNumber
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phoneNumber = phone;
+    }
 
 }
--- src/model/Table.java
+++ src/model/Table.java
@@ -3,5 +3,4 @@ package model;
 /**/
 public abstract class Table {
 
-    abstract void hi();
 }
