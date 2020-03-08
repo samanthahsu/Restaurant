d38fa4aef3ec2a94393f90a8d2acd86ed5bad2aa
jnancyy <nancy.ye@alumni.ubc.ca>
2020-03-07 22:14:52 -0800

    Customer Half is working kind of

--- src/model/FourSeater.java
+++ src/model/FourSeater.java
@@ -1,11 +1,23 @@
 package model;
 
 public class FourSeater extends Table {
+    private static final int width = 30;
+    private static final int height = 50;
 
     //EFFECTS: constructs a new two seater table with given x and y coordinate
     public FourSeater(int newX, int newY) {
         this.seats = 4;
         this.x = newX;
         this.y = newY;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public int getWidth() {
+        return width;
     }
 }
