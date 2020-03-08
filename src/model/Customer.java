9dff9d2020e49ea92a81935159aecb3ed43f6c67
feliciakwala <felicia.kwala@gmail.com>
2020-03-07 17:34:41 -0800

    TestRun 2

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,13 +1,29 @@
 package model;
 
 public class Customer {
-    String name;
-    String phoneNumber;
+    private String name;
+    private String phoneNumber;
 
     //EFFECTS: constructs a new customer with given name and phoneNumber
     public Customer(String name, String phone) {
         this.name = name;
         this.phoneNumber = phone;
     }
+
+    //EFFECTS: changes name
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    //EFFECTS: changes name
+    public void setPhone(String newPhone) {
+        this.phoneNumber = newPhone;
+    }
+
+    //EFFECTS: returns name
+    public String getName() {return name;}
+
+    //EFFECTS: returns name
+    public String getPhone() {return phoneNumber;}
 
 }
--- src/model/Table.java
+++ src/model/Table.java
@@ -1,6 +1,14 @@
 package model;
 
+
 /**/
 public abstract class Table {
+    protected int seats;
+    protected int x;
+    protected int y;
 
+    public void setPosition(int newX, int newY) {
+        this.x = newX;
+        this.y = newY;
+    }
 }
